5608eedfae1948d6564d732d9a1f55d9219747e9
Teruaki.Sato <andrea.pirlo.0529@gmail.com>
2024-03-10 07:15:26 +0900

    自動生成DDLのマイグレーション登録

---
+++ db/src/inventories/database/migrations/000002_material-tables.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS inventories.products CASCADE;
+DROP TABLE IF EXISTS inventories.company_destinations CASCADE;
+DROP TABLE IF EXISTS inventories.suppliers CASCADE;
+DROP TABLE IF EXISTS inventories.costomers CASCADE;
+DROP TABLE IF EXISTS inventories.dealing_banks CASCADE;
+DROP TABLE IF EXISTS inventories.companies CASCADE;
+
+DROP FUNCTION IF EXISTS inventories.products_audit();
+DROP FUNCTION IF EXISTS inventories.company_destinations_audit();
+DROP FUNCTION IF EXISTS inventories.suppliers_audit();
+DROP FUNCTION IF EXISTS inventories.costomers_audit();
+DROP FUNCTION IF EXISTS inventories.dealing_banks_audit();
+DROP FUNCTION IF EXISTS inventories.companies_audit();
---
+++ db/src/inventories/database/migrations/000003_material-fks.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE inventories.dealing_banks DROP CONSTRAINT dealing_banks_foreignKey_1;
+ALTER TABLE inventories.costomers DROP CONSTRAINT costomers_foreignKey_1;
+ALTER TABLE inventories.suppliers DROP CONSTRAINT suppliers_foreignKey_1;
+ALTER TABLE inventories.company_destinations DROP CONSTRAINT company_destinations_foreignKey_1;
+ALTER TABLE inventories.products DROP CONSTRAINT products_foreignKey_1;
