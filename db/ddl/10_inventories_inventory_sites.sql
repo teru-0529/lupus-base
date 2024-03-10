3e5558b74ad08d58fa2eeeb11a1658b90ff1842a
Teruaki.Sato <andrea.pirlo.0529@gmail.com>
2024-03-10 09:39:23 +0900

    倉庫テーブル追加

---
+++ db/ddl/10_inventories_inventory_sites.sql
@@ -0,0 +1,64 @@
+-- is_master_table=true
+
+-- 7.倉庫(inventory_sites)
+
+-- Create Table
+DROP TABLE IF EXISTS inventories.inventory_sites CASCADE;
+CREATE TABLE inventories.inventory_sites (
+  site_id varchar(30) NOT NULL check (LENGTH(site_id) >= 1),
+  manage_pic varchar(8) check (manage_pic ~* '^P[0-9]{7}$'),
+  allocatable boolean NOT NULL DEFAULT True,
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  updated_at timestamp NOT NULL DEFAULT current_timestamp,
+  created_by varchar(58),
+  updated_by varchar(58)
+);
+
+-- Set Table Comment
+COMMENT ON TABLE inventories.inventory_sites IS '倉庫';
+
+-- Set Column Comment
+COMMENT ON COLUMN inventories.inventory_sites.site_id IS '倉庫ID';
+COMMENT ON COLUMN inventories.inventory_sites.manage_pic IS '管理担当者ID';
+COMMENT ON COLUMN inventories.inventory_sites.allocatable IS '引当可能';
+COMMENT ON COLUMN inventories.inventory_sites.created_at IS '作成日時';
+COMMENT ON COLUMN inventories.inventory_sites.updated_at IS '更新日時';
+COMMENT ON COLUMN inventories.inventory_sites.created_by IS '作成者';
+COMMENT ON COLUMN inventories.inventory_sites.updated_by IS '更新者';
+
+-- Set PK Constraint
+ALTER TABLE inventories.inventory_sites ADD PRIMARY KEY (
+  site_id
+);
+
+-- Create 'set_update_at' Trigger
+CREATE TRIGGER set_updated_at
+  BEFORE UPDATE
+  ON inventories.inventory_sites
+  FOR EACH ROW
+EXECUTE PROCEDURE set_updated_at();
+
+-- Create 'append_history' Function
+DROP FUNCTION IF EXISTS inventories.inventory_sites_audit();
+CREATE OR REPLACE FUNCTION inventories.inventory_sites_audit() RETURNS TRIGGER AS $$
+BEGIN
+  IF (TG_OP = 'DELETE') THEN
+    INSERT INTO operation_histories(schema_name, table_name, operation_type, table_key)
+    SELECT TG_TABLE_SCHEMA, TG_TABLE_NAME, 'DELETE', OLD.site_id;
+  ELSIF (TG_OP = 'UPDATE') THEN
+    INSERT INTO operation_histories(operated_by, schema_name, table_name, operation_type, table_key)
+    SELECT NEW.updated_by, TG_TABLE_SCHEMA, TG_TABLE_NAME, 'UPDATE', NEW.site_id;
+  ELSIF (TG_OP = 'INSERT') THEN
+    INSERT INTO operation_histories(operated_by, schema_name, table_name, operation_type, table_key)
+    SELECT NEW.updated_by, TG_TABLE_SCHEMA, TG_TABLE_NAME, 'INSERT', NEW.site_id;
+  END IF;
+  RETURN null;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create 'audit' Trigger
+CREATE TRIGGER audit
+  AFTER INSERT OR UPDATE OR DELETE
+  ON inventories.inventory_sites
+  FOR EACH ROW
+EXECUTE PROCEDURE inventories.inventory_sites_audit();
--- db/src/inventories/database/migrations/000002_material-tables.down.sql
+++ db/src/inventories/database/migrations/000002_material-tables.down.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS inventories.inventory_sites CASCADE;
 DROP TABLE IF EXISTS inventories.products CASCADE;
 DROP TABLE IF EXISTS inventories.company_destinations CASCADE;
 DROP TABLE IF EXISTS inventories.suppliers CASCADE;
@@ -5,6 +6,7 @@ DROP TABLE IF EXISTS inventories.costomers CASCADE;
 DROP TABLE IF EXISTS inventories.dealing_banks CASCADE;
 DROP TABLE IF EXISTS inventories.companies CASCADE;
 
+DROP FUNCTION IF EXISTS inventories.inventory_sites_audit();
 DROP FUNCTION IF EXISTS inventories.products_audit();
 DROP FUNCTION IF EXISTS inventories.company_destinations_audit();
 DROP FUNCTION IF EXISTS inventories.suppliers_audit();
