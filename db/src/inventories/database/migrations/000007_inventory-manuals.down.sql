ffd4db0451237fe2f70d7712ba50be20d10e2ba3
Teruaki.Sato <andrea.pirlo.0529@gmail.com>
2024-03-11 08:54:01 +0900

    月次在庫サマリ＿倉庫別

--- db/src/inventories/database/migrations/000007_inventory-manuals.down.sql
+++ db/src/inventories/database/migrations/000007_inventory-manuals.down.sql
@@ -7,3 +7,5 @@ DROP FUNCTION IF EXISTS inventories.current_summaries_pre_process() CASCADE;
 ALTER TABLE inventories.inventory_histories DROP CONSTRAINT IF EXISTS inventory_histories_taransaction_type_check;
 
 DROP FUNCTION IF EXISTS inventories.inventory_histories_pre_process() CASCADE;
+
+DROP FUNCTION IF EXISTS inventories.upsert_inventory_summaries() CASCADE;
